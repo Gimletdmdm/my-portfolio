da2bf72c613c7df0a3c0413bd446373ba705ee3f
Gimletdmdm <business.dmdm@gmail.com>
2023-10-25 21:34:24 +0900

    name change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ]
 })
 export class AppComponent {
-  title = 'app-sanmple';
+  title = 'my-portfolio';
 }
